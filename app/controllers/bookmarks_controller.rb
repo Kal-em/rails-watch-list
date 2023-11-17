4e8a14296bcbdf940ad0d064a417ac3056151867
Robert Chheav <robert.chheav@gmail.com>
2023-11-17 11:14:41 +0100

    destroy bookmark functional

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -16,8 +16,9 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
